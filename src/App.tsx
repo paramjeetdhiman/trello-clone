b07953741157afdf8c5be76bbf5f352eb8df7f43
Paramjeet Dhiman <paramjeetdhiman1997@gmail.com>
2021-04-30 11:27:38 +0530

    ADD: styled-components and styles.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { AppContainer } from "./styles";
 
-function App() {
+// const buttonStyles: React.CSSProperties = {
+//   backgroundColor: "#5aac44",
+//   borderRadius: "3px",
+//   border: "none",
+// };
+
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <p>Columns will go here</p>
+    </AppContainer>
   );
 }
-
-export default App;
